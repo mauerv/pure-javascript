edda66ba76440c5720bed061bc2ec1ede5cb3884
Mauro <mauro@osn.global>
2020-05-16 23:15:03 -0300

    Add index to new list elements that I create

--- main.js
+++ main.js
@@ -4,8 +4,9 @@ const alertMessage = function () {
 
 const addElement = function () {
   const list = document.getElementById("list");
+  const newIndex = list.childNodes.length + 1;
   let li = document.createElement("li");
-  let text = document.createTextNode("New list item");
+  let text = document.createTextNode(`New list item ${newIndex}`);
   li.appendChild(text);
   list.appendChild(li);
 };
