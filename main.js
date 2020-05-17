9cc89fab75037152e1dac573da7b173962769c54
Mauro <mauro@osn.global>
2020-05-16 23:08:32 -0300

    Add button to append items to a list

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
 const alertMessage = function () {
   alert("Hello!");
 };
+
+const addElement = function () {
+  const list = document.getElementById("list");
+  let li = document.createElement("li");
+  let text = document.createTextNode("New list item");
+  li.appendChild(text);
+  list.appendChild(li);
+};
